eb362fff766dc9d7e065c660f247e6f518979be3
mendrikafitahiana <ramilisonfita24@gmail.com>
2024-04-05 16:32:11 +0300

    Création du projet TP2

---
+++ src/main/java/fr/mendrika/tpprgmendrika/config/ConfigJSF.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package fr.mendrika.tpprgmendrika.config;
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.faces.annotation.FacesConfig;
+/**
+ *
+ * @author Mendrika Fitahiana
+ */
+@ApplicationScoped
+@FacesConfig
+public class ConfigJSF {
+    
+}
